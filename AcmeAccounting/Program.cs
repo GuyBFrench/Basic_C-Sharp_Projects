e4d52ee85557e9fbb9e13de739dfba0c09df0ef4
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-01 15:37:26 -0600

    Create Program.cs

---
+++ AcmeAccounting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine(" Remember, we\'re \"accounting\" on you!");
+        }
+    }
+}
