b0d571f387349ede0c4fca1d3ea6bac9325cf07e
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-05 12:07:32 -0700

    Add daily Report C#

---
+++ Daily Report/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string techCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help? True or False.");
+            string helpInput = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today? Please enter a number such as 7.");
+            int studyHours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
