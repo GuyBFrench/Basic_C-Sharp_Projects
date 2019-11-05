1157a8aa7d63a22a7b15cb1081a528a0e118a3fe
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-05 15:19:45 -0700

    Added Basic Comparison Practice

---
+++ IncomeComparison/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Please enter your Hourly Rate.");
+            double firstHourly = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your Hours worked per Week.");
+            int firstWeek = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Please enter your Hourly Rate.");
+            double secondHourly = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your Hours worked per Week.");
+            int secondWeek = Convert.ToInt32(Console.ReadLine());
+
+            double firstSalary = firstHourly * firstWeek;
+            Console.WriteLine("Person 1 has a weekly salary of " + firstSalary + " dollars.");
+
+            double secondSalary = secondHourly * secondWeek;
+            Console.WriteLine("Person 2 has a weekly salary of " + secondSalary + " dollars.");
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            if (firstSalary > secondSalary)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
