8218ec5168a48301579ad8e4213ab6296daf4e8e
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-07 17:21:06 -0700

    Add more practices

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "My name is Benhem, ";
+            string myHome = "my home is in Texas,";
+            string myFav = " and my favorite color is green.";
+            Console.WriteLine(myName + myHome + myFav);
+            Console.ReadLine();
+
+
+            string lowerCase = "My name is Achilles";
+            string upperCase = lowerCase.ToUpper();
+            Console.WriteLine(upperCase);
+            Console.ReadLine();
+
+            StringBuilder myString = new StringBuilder();
+            myString.Append("This is a string builder.");
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
