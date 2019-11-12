b0418d4b2b547d5326353c9d3aed29d7d695500c
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-12 13:27:41 -0700

    Add Method Practice

---
+++ Methods/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    public class Math
+    {
+        public int Add(int a)
+        {
+
+            return a + 50;
+        }
+        public int Divide(int a)
+        {
+
+            return a / 2;
+        }
+        public int Subtract(int a)
+        {
+
+            return a - 10;
+        }
+
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+
+            int addResult = math.Add(userNumber);
+            Console.WriteLine("I changed your number by adding 50!! Here: " + addResult);
+
+            int divideResult = math.Divide(userNumber);
+            Console.WriteLine("I changed your number by dividing by 2! Here: " + divideResult);
+
+            int subtractResult = math.Subtract(userNumber);
+            Console.WriteLine("I changed your number by subtracting 10! Here: " + subtractResult);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
