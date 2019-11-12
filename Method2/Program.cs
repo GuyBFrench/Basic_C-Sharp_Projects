3c3c10471cede54ca5d4216a04fae762404d9e04
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-12 16:03:30 -0700

    Add More Method Practice

---
+++ Method2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void void1 = new Void();
+            void1.Bet(10, 78);
+        }
+    }
+}
---
+++ Method2/Void.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    public class Void
+    {
+        public void Bet(int A, int B)
+        {
+            int C = A * 2;
+            Console.WriteLine(B);
+            
+        }
+    }
+}
