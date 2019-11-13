4767b39f223371dc24f2d81d9befd6434b06ec18
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-13 15:05:25 -0700

    Add more method practice

---
+++ Method4/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Class1
+    {
+        public void theVoid(int A)
+        {
+            int B = A / 2;
+            Console.WriteLine(B);
+            Console.ReadLine();
+        }
+
+        static void theVoid(int A, int C)
+        {
+            int B = A / 2;
+            Console.WriteLine(B);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method4/Program.cs
@@ -0,0 +1,22 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 leVoid = new Class1();
+
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            leVoid.theVoid(userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
