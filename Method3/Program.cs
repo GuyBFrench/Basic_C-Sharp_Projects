f5bd0f213bcf0dca39cc1f05fee9de6246b202fa
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-13 11:05:45 -0700

    Add Method practice

---
+++ Method3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter how many chicken tacos you want");
+            int numChicken = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter how many fish tacos you dont have to get any.");
+            string numFish = Console.ReadLine();
+
+            Taco taco = new Taco();
+            int totalTacos;
+            Console.WriteLine("Your total number of tacos is: ");
+
+            if (Int32.TryParse(numFish, out totalTacos))
+            {
+                Console.WriteLine(taco.tacos(numChicken, totalTacos));
+            }
+            else
+            {
+                Console.WriteLine(taco.tacos(numChicken));
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
