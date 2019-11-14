b658304d3ad80c87aaef519d0d4714cd85810ec1
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-14 14:35:46 -0700

    Add Abstract Class Practice

---
+++ Abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Sample = new Employee();
+            Sample.firstName = "Sample";
+            Sample.lastName = "Student";
+
+            Sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
