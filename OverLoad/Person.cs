ca98f39693d8d3c0a5bac95ff4d0459029379794
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-15 12:54:31 -0700

    Add more Practice

---
+++ OverLoad/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        
+
+        public Person()
+        {
+            Name = "Tickle";
+            
+        }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + Name);
+        }
+    }
+}
---
+++ OverLoad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Benhem = new Employee();
+            Benhem.Name = "Benhem";
+            Employee Talbot = new Employee();
+            Talbot.Name = "Student";
+
+            bool isName = Benhem == Talbot;
+
+
+            Console.WriteLine("Is Benhem equal to Talbot?: " + isName);
+            Console.ReadLine();
+        }
+    }
+}
