0cfbd79cca5dc3298e2d54b7e1bec5b91380f1fc
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-15 16:06:34 -0700

    Added practice with generics

---
+++ 131/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Names = new Employee<string>();
+            
+            Employee<int> EmployeesId = new Employee<int>();
+
+            Names.things.Add("Benhem");
+            Names.things.Add("Talbot");
+            Names.things.Add("Pete");
+            Names.things.Add("Kate");
+
+            EmployeesId.things.Add(23);
+            EmployeesId.things.Add(22);
+            EmployeesId.things.Add(21);
+            EmployeesId.things.Add(26);
+            
+            foreach (string item in Names.things)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int item in EmployeesId.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
