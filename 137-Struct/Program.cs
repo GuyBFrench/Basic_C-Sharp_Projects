647ebd863c715572b664c045c54ab2bc2412f083
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-19 11:45:15 -0700

    Added a Struct practice

---
+++ 137-Struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _137_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+
+            number.Amount = 12;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    public struct Number
+    {
+        public decimal Amount;
+    }
+}
