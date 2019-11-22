14c5d1418d7c58bdb8fa5d4a596301ff129fa5e0
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-22 15:03:12 -0700

    Add a dateTime practice

---
+++ 156-DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _156_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            int userHours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime timeNow = DateTime.Now;
+            DateTime addHours = timeNow.AddHours(userHours);
+            Console.WriteLine("In that many hours it will be: " + addHours);
+
+            Console.ReadLine();
+
+        }
+    }
+}
