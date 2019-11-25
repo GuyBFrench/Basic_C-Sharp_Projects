d06c98324744f4fd752a393cb00e05fbc11ae0cd
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-11-25 12:18:38 -0700

    Added a practice

---
+++ 162-Const/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _162_Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string programName = "The Program";
+
+            var theList = new Dictionary<string, string>();
+        }
+    }
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Name { get; set; }
+
+        public People(string firstName, string lastName)
+        {
+            Name = firstName + lastName; 
+        }
+        public People(string firstName) : this(firstName, "Johnson")
+        {
+        }
+    }
+}
