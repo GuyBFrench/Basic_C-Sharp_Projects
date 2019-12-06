8b3067752bd482583b40260b18c695c0fa9b0c38
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2019-12-06 11:40:50 -0700

    Final Project

---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {                
+                Console.WriteLine("Enter the name for your new blog: ");
+                var name = Console.ReadLine();
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                var query = from b in db.Blogs orderby b.Name select b;
+
+                Console.WriteLine("All of the blogs in DataBase: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+    }
+}
