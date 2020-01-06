a75f15accccac0d156e8ff577ae742a613e96d88
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2020-01-06 15:55:08 -0600

    Fixed OverLoad

---
+++ OverloadRE/OverloadRE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadRE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Benhem = new Employee();
+            Employee employee2 = new Employee();
+
+            employee2.Id = 0001;
+            employee2.Name = "Talbot";
+
+            Benhem.Id = 0002;
+            Benhem.Name = "Benhem";
+
+            bool isEmployee = Benhem == employee2;
+
+            Console.WriteLine("Does " + Benhem.Name + " and " + employee2.Name + "s employee Ids match? " + isEmployee);
+            Console.ReadLine();
+        }
+    }
+}
